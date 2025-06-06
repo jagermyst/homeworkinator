9fa07c8b038ad4d918f4c1daff88c4d850ab1da0
jagermyst <120776655+jagermyst@users.noreply.github.com>
2025-06-05 22:06:05 -0600

    Created main class
    just got the project started, have not implimented anything yet

---
+++ homework_inator/src/Homeworkinator.java
@@ -0,0 +1,31 @@
+// Created by Jagermyst
+
+public class Homeworkinator {
+
+    static void createHomework() {
+        System.out.println("Homework");
+    }
+
+    static void createStudent() {
+        System.out.println("what is the new students name?");
+    }
+    static void createSubject() {
+        System.out.println("Subject");
+    }
+
+    static void studentReport() {
+        System.out.println("Which student would you like a report on?");
+    }
+
+            public static void main(String[] args) {
+                System.out.println("Welcome to the HOMEWORKINATOR!");
+                System.out.println("please choose what you would like to do today:");
+                System.out.println("1. create a study assignment");
+                System.out.println("2. get a report on a student");
+                System.out.println("3. create a new student");
+                System.out.println("4. create a new subject of homework");
+                System.out.println("5. delete a student");
+                createHomework();
+            }
+        }
+
