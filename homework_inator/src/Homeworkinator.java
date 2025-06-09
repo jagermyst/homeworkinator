65b660747917442ba1942b6cab2f36ac4fb8ef5d
jagermyst <120776655+jagermyst@users.noreply.github.com>
2025-06-09 15:45:39 -0600

    started implimentation of user input
    created the HwFactory class and implimented the user input. exit function needs debugging and methods need be implimented still

--- homework_inator/src/Homeworkinator.java
+++ homework_inator/src/Homeworkinator.java
@@ -1,9 +1,12 @@
 // Created by Jagermyst
+import java.util.Scanner;
 
 public class Homeworkinator {
 
     static void createHomework() {
-        System.out.println("Homework");
+        //TODO for now this will have no args; this will need to be changed to ask the
+        // user which student we are making homework for
+        HwFactory hwFactory = new HwFactory();
     }
 
     static void createStudent() {
@@ -17,15 +20,64 @@ public class Homeworkinator {
         System.out.println("Which student would you like a report on?");
     }
 
+    static String stringCleaner(String userInput) {
+        //TODO make this method sanitize user input by removing spaces, etc.
+        return userInput;
+    }
+
+    static boolean selector(boolean exit) {
+        // initalize scanner and ask user for input
+        Scanner input = new Scanner(System.in);
+
+        String selection = input.nextLine();
+        selection = stringCleaner(selection);
+
+        // calls a method based on the users selection
+        switch (selection) {
+            case "1":
+                //TODO: prompt user for a topic or student in here
+                createHomework();
+                break;
+            case "2":
+                System.out.println("TODO: print a report on the student");
+                break;
+            case "3":
+                System.out.println("TODO: create a new student");
+                break;
+            case "4":
+                System.out.println("TODO: CREATE A NEW TOPIC FILE");
+                break;
+            case "5":
+                System.out.println("TODO: DELETE A STUDENT FILE");
+                break;
+            case "6":
+                //TODO: FIGURE OUT WHY THIS IS NOT EXITING THE PROGRAM
+                exit = true;
+                break;
+            default:
+                System.out.println("please type in a number 1-6");
+                break;
+        }
+        return exit;
+
+    }
+
+
             public static void main(String[] args) {
+                // prompt asking for a selection
                 System.out.println("Welcome to the HOMEWORKINATOR!");
-                System.out.println("please choose what you would like to do today:");
+                System.out.println("please choose what you would like to do today (1-6):");
                 System.out.println("1. create a study assignment");
                 System.out.println("2. get a report on a student");
                 System.out.println("3. create a new student");
                 System.out.println("4. create a new subject of homework");
                 System.out.println("5. delete a student");
-                createHomework();
+                System.out.println("6. exit program");
+                boolean exit = false;
+                while (!exit){
+                    exit = selector(exit);
+                }
+
             }
         }
 
---
+++ homework_inator/src/HwFactory.java
@@ -0,0 +1,9 @@
+public class HwFactory {
+    //The default HwFactory will be exclusively for aviation related topics, later we can make it compatible with multiple topics
+    public  HwFactory(){
+        System.out.println("hw factory no args");
+    }
+    public HwFactory(String student){
+        System.out.println("hw factory 1 arg for ");
+    }
+}
