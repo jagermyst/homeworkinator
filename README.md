20408c2d4d970ced5fd02bdba8b5c199d1f91669
jagermyst <120776655+jagermyst@users.noreply.github.com>
2025-06-12 16:09:30 -0600

    file reading initial implimentation
    the initial implementation for reading files in homework factory. cannot currently find the right file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # homework_inator
  a program that creates homework assignments
+
+ ## requirements spec. ##
+
+ ## psuedocode ##
+    (see paper docs for full uml and psudocode)
+ ## implimentation ##
+1. homework factory must find the file and read it... it currently does not
+ ## known issuea/debugging ##
+1. program exit feature does not work
+
--- homework_inator/src/HwFactory.java
+++ homework_inator/src/HwFactory.java
@@ -1,7 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class HwFactory {
+
+    static String readFile() throws FileNotFoundException {
+        File file = new File("\"C:\\Users\\jager\\Documents\\projects\\Java\\Homework_inator\\homework_inator\\topics\\all_aviation.txt\"");
+        Scanner fileReader = new Scanner(file);
+        StringBuilder fileData = new StringBuilder();
+        while (fileReader.hasNextLine()){
+            fileData.append(fileReader.nextLine());
+        }
+        return (fileData.toString());
+    }
+
+
+
+
+
+
     //The default HwFactory will be exclusively for aviation related topics, later we can make it compatible with multiple topics
-    public  HwFactory(){
-        System.out.println("hw factory no args");
+    public  HwFactory() {
+        try {
+            System.out.println("hw factory no args " + readFile());
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            exit(1);
+        }
+
+
     }
     public HwFactory(String student){
         System.out.println("hw factory 1 arg for ");
